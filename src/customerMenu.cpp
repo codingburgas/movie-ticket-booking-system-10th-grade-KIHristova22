ff1c208b9cca8677f995fa53e926d29b120b3b92
KIHristova22 <KIHristova22@codingburgas.bg>
2025-06-22 13:55:12 +0300

    Add a function to book and view tickets

--- src/customerMenu.cpp
+++ src/customerMenu.cpp
@@ -18,3 +18,37 @@ void initializeMovies() {
 	movies[3] = { 4, "Pirates of the Carribean", 100, 0 };
 	movies[4] = { 5, "Lord of the rings", 100, 0 };
 }
+
+void viewMovies() {
+	cout << "Available Movies:" << endl;
+	for (int i = 0; i < 5; ++i) {
+		cout << movies[i].movieNum << ". " << movies[i].name
+			<< " - Available Seats: " << movies[i].seat
+			<< ", Booked: " << movies[i].bookedSeats << endl;
+	}
+}
+
+void bookTickets() {
+    int choice, tickets;
+    viewMovies();
+    cout << "Enter the movie number to book tickets for: ";
+    cin >> choice;
+
+    if (choice < 1 || choice > 5) {
+        cout << "Invalid movie number!" << endl;
+        return;
+    }
+
+    movieDetail& movie = movies[choice - 1];
+    cout << "How many tickets would you like to book for " << movie.name << "? ";
+    cin >> tickets;
+
+    if (tickets <= 0 || tickets > movie.seat) {
+        cout << "Invalid number of tickets!" << endl;
+        return;
+    }
+
+    movie.seat -= tickets;
+    movie.bookedSeats += tickets;
+    cout << "Tickets booked successfully for " << movie.name << "!" << endl;
+}
